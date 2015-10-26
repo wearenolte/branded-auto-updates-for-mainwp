1ae0e6be268a55ed9531639ed4d4b4ed8aba5746
Dominique Mariano <powered.by.coffee.and.cream@gmail.com>
2015-10-26 23:02:11 +0800

    Add sample tests file to pacify travis.

---
+++ tests/test-sample.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This is just a sample test to tame Travis. Actual unit tests are to be
+ * written as code stabilizes.
+ */
+class SampleTest extends WP_UnitTestCase {
+	function testSample() {
+		$this->assertTrue( true );
+	}
+}
